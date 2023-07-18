3b04fa35865e4f3ed10cd67648c09e60602ca477
olakunle-remi <abbeypentious@gmail.com>
2023-07-18 05:01:16 +0000

    0x0C-python-almost_a_circle update

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+from models.base import Base
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Width must be an integer")
+        if value <= 0:
+            raise ValueError("Width must be greater than 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Height must be an integer")
+        if value <= 0:
+            raise ValueError("Height must be greater than 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise ValueError("X must be an integer")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Y must be an integer")
+        self.__y = value
+
