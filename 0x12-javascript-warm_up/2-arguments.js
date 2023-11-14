19b68e2cdba3bce73e76af5f1db2f352986beeac
olakunle-remi <abbeypentious@gmail.com>
2023-11-13 21:50:08 -0800

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+const argulength = process.argv.length;
+if (argulength === 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (argulength === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
-
